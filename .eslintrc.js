b9bfc00918a69fa8523e39f4fac564f55d39e546
Johnny Almonte <johnny243@users.noreply.github.com>
2021-01-19 19:04:17 -0400

    feat: eslint for linting codebase

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "amd": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "rules": {
+    "indent": [2, 2],
+    "no-var": "error"
+  }
+};
